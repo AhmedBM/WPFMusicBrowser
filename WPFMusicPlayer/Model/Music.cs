0711c302a76cb3dc26e0b325b387c32edc8b6173
Ahmed Messaoud <ahmed.messaoud@magorcorp.com>
2014-10-14 20:28:04 -0400

    Added skeleton

---
+++ WPFMusicPlayer/Model/Music.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMusicPlayer.Model
+{
+    class Artist
+    {
+        #region Members
+        public string _name;
+        #endregion
+    }
+
+    class Album
+    {
+        //"title": "The King of Limbs",
+        //  "image": "http://upload.wikimedia.org/wikipedia/en/2/24/The_king_of_limbs.jpg",
+        //  "description": "The King of Limbs is the eighth studio album by English rock band Radiohead, produced by Nigel Godrich. It was self-released on 18 February 2011 as a download in MP3 and WAV formats, followed by physical CD and 12' vinyl releases on 28 March, a wider digital release via AWAL, and a special 'newspaper' edition on 9 May 2011. The physical editions were released through the band's Ticker Tape imprint on XL in the United Kingdom, TBD in the United States, and Hostess Entertainment in Japan.",
+        //  "date": "2012-04-23T18:25:43.511Z",
+        #region Members
+        public string _title;
+        public string _description;
+        public string _imageUri;
+        public DateTime _date;
+        #endregion
+    }
+
+    class Song
+    {
+        #region Members
+        public string _title;
+        public TimeSpan _length;
+        #endregion
+    }
+}
---
+++ WPFMusicPlayer/Model/MusicViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFMusicPlayer.Model
+{
+    class ArtistViewModel : BaseViewModel
+    {
+        #region Members
+        private Artist _artist;
+        private ObservableCollection<AlbumViewModel> _albums; 
+        #endregion
+
+        #region Constructor
+        public ArtistViewModel()
+        {
+            _artist = new Artist();
+        }
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get { return _artist._name; }
+            set
+            {
+                _artist._name = value;
+                RaisePropertyChangedEvent("Name");
+            }
+        }
+        #endregion
+    }
+
+    class AlbumViewModel : BaseViewModel
+    {
+        #region Members
+        private Album _album;
+        private ObservableCollection<SongtViewModel> _songs;
+        #endregion
+
+        #region Constructor
+        public AlbumViewModel()
+        {
+            _album = new Album();
+        }
+        #endregion
+
+        #region Properties
+        public string Title
+        {
+            get { return _album._title; }
+            set
+            {
+                _album._title = value;
+                RaisePropertyChangedEvent("Title");
+            }
+        }
+        public string Description
+        {
+            get { return _album._description; }
+            set
+            {
+                _album._description = value;
+                RaisePropertyChangedEvent("Description");
+            }
+        }
+        public DateTime Date
+        {
+            get { return _album._date; }
+            set
+            {
+                _album._date = value;
+                RaisePropertyChangedEvent("Date");
+            }
+        }
+        public string ImageUri
+        {
+            get { return _album._imageUri; }
+            set
+            {
+                _album._imageUri = value;
+                RaisePropertyChangedEvent("ImageUri");
+            }
+        }
+        #endregion
+    }
+
+    class SongtViewModel : BaseViewModel
+    {
+        #region Members
+        private Song _song;
+        #endregion
+
+        #region Constructor
+        public SongtViewModel()
+        {
+            _song = new Song();
+        }
+        #endregion
+
+        #region Properties
+        public string Title
+        {
+            get { return _song._title; }
+            set
+            {
+                _song._title = value;
+                RaisePropertyChangedEvent("Title");
+            }
+        }
+        public TimeSpan Length
+        {
+            get { return _song._length; }
+            set
+            {
+                _song._length = value;
+                RaisePropertyChangedEvent("Length");
+            }
+        }
+        #endregion
+    }
+}
